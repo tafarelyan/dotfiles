3700c500cc94e6f8b23f9320430eef4fe31df5d6
Tafarel Yan <tafarel.yan@gmail.com>
2016-07-04 11:20:27 -0300

    New alias and easy moving

--- tohome.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-import subprocess
-
-subprocess.call('cp vimrc ~/.vimrc', shell=True)
-subprocess.call('cp zshrc ~/.zshrc', shell=True)
-subprocess.call('cp tmux.conf ~/.tmux.conf', shell=True)
-subprocess.call('cp vimrc.bundles ~/.vimrc.bundles', shell=True)
-subprocess.call('cp aliases ~/.aliases', shell=True)
