833ebf68a3126dd5ffbfbe6d53baf7d019b7889f
Tafarel Yan <tafarel.yan@gmail.com>
2020-02-17 10:08:32 -0300

    Using copy command instead of move

--- move_to_home_folder.py
+++ move_to_home_folder.py
@@ -1,14 +1,14 @@
 import os
 import shutil
 
-shutil.move(
+shutil.copyfile(
     os.path.join(os.getcwd(), 'vimrc'),
     os.path.join(os.path.expanduser('~'), '.vimrc')
 )
 
-shutil.move(
+shutil.copyfile(
     os.path.join(os.getcwd(), 'zshrc'),
     os.path.join(os.path.expanduser('~'), '.zshrc')
 )
 
-print('Moved dotfiles to home folder')
+print('Copied dotfiles to home folder')
